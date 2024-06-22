8e8e29e0de810ae49970acefb657eae1463ab87d
maayan <yogevm15@gmail.com>
2024-06-22 22:15:46 +0300

    More reexports

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@
 #![allow(clippy::multiple_crate_versions)]
 #![allow(clippy::missing_errors_doc)]
 pub use error::{Error, Result};
-pub use protocol::{responses::card_punch::CardPunch, CardOwnerData, CardReadout};
+pub use protocol::{
+    responses::card::{Card, CardType},
+    responses::card_punch::CardPunch,
+    CardOwnerData, CardReadout, DayOfWeek, DecoderError, EncoderError, Punch, StartOrFinishPunch,
+    SubSecondPunch, WeekCounter,
+};
 pub use reader::Reader;
 mod error;
 mod protocol;
